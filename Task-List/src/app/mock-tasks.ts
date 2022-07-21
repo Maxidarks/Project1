f169a5b1df145ba6e3d986f0a5d3a903dfaef1c1
Maxidarks <ahumadamaxi175@gmail.com>
2022-07-21 18:22:18 -0300

    add mock-task

---
+++ Task-List/src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASK:Task[] = [
+    {
+        id: 1,
+        text: "End first module in Angular",
+        day:  'July 22, 10:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Work",
+        day:  'July 22, 11:30',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Eat",
+        day:  'July 22, 13:00',
+        reminder: false,
+    },
+    {
+        id: 4,
+        text: "Sport",
+        day:  'July 22, 17:00',
+        reminder: false,
+    },
+]
